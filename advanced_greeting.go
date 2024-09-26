d7ab43307e33b509bd3f4555c2f1d501a7962c5c
--replace-all <amanssinha9@gmail.com>
2024-09-26 14:52:07 -0400

    adding all the pending go func

--- advanced_greeting.go
+++ advanced_greeting.go
@@ -2,16 +2,6 @@ package main
 
 import "fmt"
 
-func main() {
-	Greeting()
-	advanceGreeting("Aman")
-	advanceGreeting("Azure")
-}
-
-func Greeting() {
-	fmt.Println("Hey There !!")
-}
-
 func advanceGreeting(name string) {
 	fmt.Printf("Hello, %s!\n", name)
 }
--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
-func main () {
-	fmt.Println("Hello, World")
+func main() {
+	Greeting()
+	advanceGreeting("Aman")
+	advanceGreeting("Sinha")
+	fmt.Println(add(1, 2))
+	add(1, 2)
+	q, r := divide(10, 3)
+	fmt.Printf("Quotient: %d, REminder: %d\n", q, r)
+	q, r = 20, 3
+	fmt.Printf("Quotient: %d, REminder: %d\n", q, r)
 }
